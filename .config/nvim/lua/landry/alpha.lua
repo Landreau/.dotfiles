aa7f1b4c8c98e38e8633d426b9e5a1fcf4c7297b
Landreau <kalipelandry00228@gmail.com>
2024-02-09 07:49:04 +0100

    Update

---
+++ .config/nvim/lua/landry/alpha.lua
@@ -0,0 +1,68 @@
+local M = {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+}
+
+function M.config()
+  local dashboard = require "alpha.themes.dashboard"
+  local icons = require "landry.icons"
+
+  local function button(sc, txt, keybind, keybind_opts)
+    local b = dashboard.button(sc, txt, keybind, keybind_opts)
+    b.opts.hl_shortcut = "Include"
+    return b
+  end
+
+  dashboard.section.header.val = {
+    [[ ★　✯   🛸                                     🪐   .°•                        ]],
+    [[  ° ★　•       🛰                     ° ★　•       /                           ]],
+    [[  __   __   __   __    __       ______   __   __   _____    ______   __  __    ]],
+    [[ /\ "-.\ \ /\ \ / /   /\ \     /\  __ \ /\ "-.\ \ /\  __-. /\  == \ /\ \_\ \   ]],
+    [[ \ \ \-.  \\ \ \'/    \ \ \____\ \  __ \\ \ \-.  \\ \ \/\ \\ \  __< \ \____ \  ]],
+    [[  \ \_\\"\_\\ \__|     \ \_____\\ \_\ \_\\ \_\\"\_\\ \____- \ \_\ \_\\/\_____\ ]],
+    [[   \/_/ \/_/ \/_/       \/_____/ \/_/\/_/ \/_/ \/_/ \/____/  \/_/ /_/ \/_____/ ]],
+  }
+
+  dashboard.section.buttons.val = {
+    button("f", icons.ui.Files .. " Find file", ":Telescope find_files <CR>"),
+    button("n", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
+    button("p", icons.git.Repo .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+    button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
+    button("t", icons.ui.Text .. " Find text", ":Telescope live_grep <CR>"),
+    button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+    button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
+  }
+  local function footer()
+    return "landryx-kal.com"
+  end
+
+  dashboard.section.footer.val = footer()
+
+  dashboard.section.header.opts.hl = "Keyword"
+  dashboard.section.buttons.opts.hl = "Include"
+  dashboard.section.footer.opts.hl = "Type"
+
+  dashboard.opts.opts.noautocmd = true
+  require("alpha").setup(dashboard.opts)
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyVimStarted",
+    callback = function()
+      local stats = require("lazy").stats()
+      local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+      dashboard.section.footer.val = "Loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+      pcall(vim.cmd.AlphaRedraw)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = { "AlphaReady" },
+    callback = function()
+      vim.cmd [[
+      set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
+    ]]
+    end,
+  })
+end
+
+return M
